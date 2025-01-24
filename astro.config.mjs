fadaf58f03b310ad291113cc1db33b5541a830bc
xtian7489 <xtian7489@gmail.com>
2025-01-24 13:17:07 -0300

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-
-  integrations: [tailwind()]
+  site: 'https://democraciaenred.github.io/',
+  base: 'periodistas-amenazadas',
+  integrations: [tailwind(), icon()]
 });
