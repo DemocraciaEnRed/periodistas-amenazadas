95b49ddc83d34e15c6dcc8de1efc4337ba2015bd
xtian7489 <xtian7489@gmail.com>
2025-02-11 18:16:12 -0300

    fix astro config to githubpaages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-
+  site: 'https://democraciaenred.github.io/',
+  base: 'periodistas-amenazadas',
   integrations: [tailwind(), icon()]
 });
