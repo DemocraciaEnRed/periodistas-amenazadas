e0b62947af67054217d62a315211f2f012e31f3c
Pano <marianovaldez92@protonmail.com>
2025-03-05 16:02:28 -0300

    finalizo graficos de sección encuestadas

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -102,6 +102,77 @@ export const datosAutores = {
   ],
 };
 
+export const categoriasPersonalesEncuestadas = [
+  { name: "Edad de la encuestada", slug: "edad" },
+  { name: "Nivel educativo", slug: "educacion" },
+  { name: "Jefa de hogar", slug: "jefa" },
+  { name: "Cantidad de hijos", slug: "hijos" }
+];
+
+export const datosPersonales = {
+  edad: [
+    { name: "Entre 18 y 25", value: 4 },
+    { name: "Entre 26 y 35", value: 49 },
+    { name: "Entre 36 y 44", value: 71 },
+    { name: "Entre 45 y 60", value: 84 },
+    { name: "Entre 61 y 80", value: 7 },
+  ],
+  educacion: [
+    { name: "Secundario completo/incompleto", value: 2 },
+    { name: "Terciario completo", value: 25 },
+    { name: "Universitario incompleto", value: 62 },
+    { name: "Universitario completo", value: 78 },
+    { name: "Posgrado incompleto", value: 21 },
+    { name: "Posgrado completo", value: 27 },
+  ],
+  jefa: [
+    { name: "Sí", value: 133 },
+    { name: "No", value: 82 },
+  ],
+  hijos: [
+    { name: "Ninguno", value: 91 },
+    { name: "1 hijo/a", value: 46 },
+    { name: "2 hijos/as", value: 51 },
+    { name: "3 hijos/as", value: 23 },
+    { name: "4 hijos/as", value: 2 },
+    { name: "5 hijos/as", value: 2 },
+  ]
+};
+
+
+export const categoriasProfesionalesEncuestadas = [
+  { name: "Años de ejercicio de la profesión", slug: "anos" },
+  { name: "Ámbito de ejercicio de la profesión", slug: "ambito" },
+  { name: "Plataforma en la que se desarrolla", slug: "plataforma" },
+];
+
+export const datosProfesionales = {
+  anos: [
+    { name: "0 a 5 años", value: 28 },
+    { name: "6 a 10 años", value: 28 },
+    { name: "11 a 15 años", value: 27 },
+    { name: "16 a 20 años", value: 41 },
+    { name: "Más de 20 años", value: 91 },
+  ],
+  ambito: [
+    { name: "Público", value: 29 },
+    { name: "Privado", value: 140 },
+    { name: "Público y privado", value: 46 }
+
+  ],
+  plataforma: [
+    { name: "Medio gráfico", value: 80 },
+    { name: "Medio audiovisual", value: 53 },
+    { name: "Medio radiofónico", value: 85 },
+    { name: "Fotoperiodismo", value: 4 },
+    { name: "Medio multiplataforma", value: 108 },
+    { name: "Perfil personal en redes sociales", value: 37 },
+    { name: "Podcast", value: 1 },
+    { name: "Institucional", value: 1 },
+    { name: "Newsletter", value: 1 }
+  ]
+};
+
 export const colors = [
   "#401F71",
   "#88659F",
